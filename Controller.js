edcf530b8edba3785446cf631f0f484cc2867103
wty13 <wetyamamoto@gmail.com>
2021-08-28 09:35:26 -0300

    desafio

--- Controller.js
+++ Controller.js
@@ -15,14 +15,10 @@ app.get('/', function(req,res){
     res.send('Olá mundo!');
 });
 
-app.get('/clientes', async(req,res)=>{
-    let create=await cliente.create({
-        nome: "Jesus",
-        endereco: "Rua centro",
-        cidade: "Marialva",
-        uf: "PR",
-        nascimento:"2021-08-27", 
-    });
+app.post('/clientes', async(req,res)=>{
+    let create=await cliente.create(
+        req.body
+    );
     res.send('Cliente foi inserido!');
 });
 
@@ -33,13 +29,10 @@ app.post('/servicos', async(req,res)=>{
     res.send('Serviço foi inserido!');
 });
 
-app.get('/pedidos', async(req,res)=>{
-    let create=await pedido.create({
-        idCliente: "1",
-        idServico: "1",
-        valor: "1",
-        data: "2021-08-27",
-    });
+app.post('/pedidos', async(req,res)=>{
+    let create=await pedido.create(
+        req.body
+    );
     res.send('Pedido foi inserido!');
 });
 
